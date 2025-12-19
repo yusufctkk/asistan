fa2bc9c05db79c7013cd60d183eb0041580968db
yusufctkk <127448746+yusufctkk@users.noreply.github.com>
2025-12-19 17:53:58 +0300

    Update service worker for cache management and fetch

--- sw.js
+++ sw.js
@@ -1,16 +1,23 @@
-
-const CACHE_NAME = 'elysian-v4';
-const assets = ['index.html', 'manifest.json'];
+const CACHE_NAME = 'elysian-v5'; // Versiyonu artırdık
 
 self.addEventListener('install', (e) => {
-  e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(assets)));
+  self.skipWaiting();
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(['index.html', 'manifest.json']))
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.filter((cacheName) => cacheName !== CACHE_NAME)
+                  .map((cacheName) => caches.delete(cacheName))
+      );
+    })
+  );
 });
 
 self.addEventListener('fetch', (e) => {
-  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
-});
-
-self.addEventListener('notificationclick', (e) => {
-  e.notification.close();
-  e.waitUntil(clients.openWindow('/'));
+  e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
 });
