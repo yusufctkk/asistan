7c1df938f6ba5a11bff152c91de1cfb63e331385
yusufctkk <127448746+yusufctkk@users.noreply.github.com>
2025-12-19 16:47:45 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,16 @@
-self.addEventListener('fetch', function(event) {});
+
+const CACHE_NAME = 'elysian-v4';
+const assets = ['index.html', 'manifest.json'];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(assets)));
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
+});
+
+self.addEventListener('notificationclick', (e) => {
+  e.notification.close();
+  e.waitUntil(clients.openWindow('/'));
+});
