483c9f25728e648c1215161d8d0f25ba4df079a6
yusufctkk <127448746+yusufctkk@users.noreply.github.com>
2025-12-20 13:37:40 +0300

    Update service worker for caching and activation

--- sw.js
+++ sw.js
@@ -1,31 +1,8 @@
-const CACHE_NAME = 'elysian-v5-final';
-const URLS_TO_CACHE = [
-  './',
-  './index.html',
-  './manifest.json',
-  'https://cdn.tailwindcss.com',
-  'https://unpkg.com/@babel/standalone/babel.min.js'
-];
 
-self.addEventListener('install', (event) => {
-  self.skipWaiting();
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(URLS_TO_CACHE))
-  );
+const CACHE_NAME = 'elysian-v8-absolute';
+self.addEventListener('install', e => { self.skipWaiting(); });
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys => Promise.all(keys.map(k => caches.delete(k)))));
+  self.clients.claim();
 });
-
-self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    caches.match(event.request).then((response) => response || fetch(event.request))
-  );
-});
-
-self.addEventListener('activate', (event) => {
-  event.waitUntil(
-    caches.keys().then((cacheNames) => Promise.all(
-      cacheNames.map((name) => {
-        if (name !== CACHE_NAME) return caches.delete(name);
-      })
-    ))
-  );
-});
+self.addEventListener('fetch', e => e.respondWith(fetch(e.request).catch(()=>caches.match(e.request))));
